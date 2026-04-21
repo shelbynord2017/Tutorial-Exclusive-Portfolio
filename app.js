da58bcdcf2da686c48e3a816fc9e41d97de894f0
Shelbynord2017 <shelbynord2017@gmail.com>
2026-04-21 14:09:48 -0400

    dark-theme

--- app.js
+++ app.js
@@ -2,34 +2,52 @@
 // service_rqanuq8
 // OXbkIROvgSe0HUYwQ
 
+let isModalOpen = false
+
+function toggleContrast() {
+  document.body.classList.toggle("dark-theme")
+}
+
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible"
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
 
-    emailjs
-        .sendForm(
-            'service_rqanuq8',
-            'template_s3chdi9',
-            event.target,
-            'OXbkIROvgSe0HUYwQ'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible"
-    }) .catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert (
-          "The email service is temporarily unavailable. Please contact me directly at shelbynord2017@gmail.com."
-        )
+  emailjs
+    .sendForm(
+      "service_rqanuq8",
+      "template_s3chdi9",
+      event.target,
+      "OXbkIROvgSe0HUYwQ",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+      success.style.display = "flex";
     })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at shelbynord2017@gmail.com.",
+      );
+    });
 
-function submitForm() {
-  setTimeout(() => {
-    submissionStatus = "submitted";
-    console.log("Form has been submitted.");
-    document.querySelector(".modal__overlay--success").style.display = "flex";
-  }, 2000); // Simulate form submission delay
-}
+  function submitForm() {
+    setTimeout(() => {
+      submissionStatus = "submitted";
+      console.log("Form has been submitted.");
+      document.querySelector(".modal__overlay--success").style.display = "flex";
+    }, 2000); // Simulate form submission delay
+  }
 }
 
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open")
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open"
+}
